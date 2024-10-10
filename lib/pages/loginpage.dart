f99f93aa9e4198b12d743d46662c0ae88a4a841b
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-10-10 22:02:12 +0545

    adding homepage

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_chat_app/auth/auth_service.dart';
 import 'package:simple_chat_app/components/My_button.dart';
 import 'package:simple_chat_app/components/my_textfield.dart';
 
@@ -12,7 +13,25 @@ class LoginPage extends StatelessWidget {
   LoginPage({super.key, required this.onTap});
 
   //login method
-  void login() {}
+  void login(BuildContext context) async {
+    //auth service
+    final authService = AuthService();
+
+    //try login
+    try {
+      await authService.signInWithEmailPassword(
+        _emailController.text,
+        _passController.text,
+      );
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(e.toString()),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +77,7 @@ class LoginPage extends StatelessWidget {
                 ),
                 MyButton(
                   text: "Login",
-                  onTap: login,
+                  onTap: () => login(context),
                 ),
                 const SizedBox(
                   height: 25,
