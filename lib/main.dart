1b8b8d593966401c5fbdaa89ceb3f6292a624f27
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-10-12 21:47:28 +0545

    solving error

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_chat_app/auth/auth_gate.dart';
 import 'package:simple_chat_app/auth/login_or_register.dart';
 import 'package:simple_chat_app/firebase_options.dart';
 import 'package:simple_chat_app/pages/loginpage.dart';
@@ -8,8 +9,7 @@ import 'package:simple_chat_app/themes/light_mode.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
-      home: LoginOrRegister(),
+      home: const AuthGate(),
       theme: lightMode,
     );
   }
