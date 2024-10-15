676116537f6fc62712f31fde9f01d087351adea1
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-10-15 22:29:18 +0545

    adding drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_chat_app/auth/auth_service.dart';
+import 'package:simple_chat_app/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,6 +19,7 @@ class HomePage extends StatelessWidget {
         ),
         actions: [IconButton(onPressed: logout, icon: Icon(Icons.logout))],
       ),
+      drawer: MyDrawer(),
     );
   }
 }
