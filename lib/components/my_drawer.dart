1ff9efa9948e0f944d2bbd69319c676f6d6cf851
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-10-16 22:12:10 +0545

    adding different logo

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -9,9 +9,41 @@ class MyDrawer extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       child: Column(
         children: [
+          //logo
           DrawerHeader(
             child: Center(
-              child: Icon(Icons.message),
+              child: Icon(
+                Icons.message,
+                color: Theme.of(context).colorScheme.primary,
+                size: 40,
+              ),
+            ),
+          ),
+          //home listTile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              title: Text("HOME"),
+              leading: Icon(Icons.home),
+              onTap: () {},
+            ),
+          ),
+          //setting listtile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              title: Text("SETTINGS"),
+              leading: Icon(Icons.settings),
+              onTap: () {},
+            ),
+          ),
+          //logout listtile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              title: Text("LOGOUT"),
+              leading: Icon(Icons.logout),
+              onTap: () {},
             ),
           ),
         ],
