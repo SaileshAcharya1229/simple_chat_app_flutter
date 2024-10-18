7d6a89f5cb2a5af1c3a653d860639b98109d5d50
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-10-19 05:14:27 +0545

    adding chatservices

--- lib/services/auth/auth_gate.dart
+++ lib/services/auth/auth_gate.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:simple_chat_app/auth/login_or_register.dart';
+import 'package:simple_chat_app/services/auth/login_or_register.dart';
 import 'package:simple_chat_app/pages/home_page.dart';
 
 class AuthGate extends StatelessWidget {
