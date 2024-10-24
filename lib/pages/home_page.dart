fc05c5450976f27f99f4661de3b33f10adf18dbc
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-10-24 23:19:08 +0545

    adding chatservices

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:simple_chat_app/components/my_drawer.dart';
+import 'package:simple_chat_app/components/user_tile.dart';
+import 'package:simple_chat_app/services/auth/auth_service.dart';
+import 'package:simple_chat_app/services/chat/chat_services.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
- 
+//chat & auth service
+  final ChatServices _chatServices = ChatServices();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +20,34 @@ class HomePage extends StatelessWidget {
         ),
       ),
       drawer: MyDrawer(),
+      body: _buildUserList(),
     );
   }
+
+  //build a list of users except for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatServices.getUsersStream(),
+      builder: (context, snapshot) {
+        //error
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        //loading..
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        //return ListView
+        return ListView(
+          children: snapshot.data!
+              .map<Widget>((userData) => _buildUserListItem)
+              .toList(),
+        );
+      },
+    );
+  }
+
+ 
 }
