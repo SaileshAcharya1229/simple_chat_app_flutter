57947971f3d7fc3bf562acf869746f0562a9f793
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-11-03 23:47:58 +0545

    adding functionality regards chat

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_chat_app/services/auth/auth_service.dart';
 import 'package:simple_chat_app/services/chat/chat_services.dart';
@@ -11,7 +12,7 @@ class ChatPage extends StatelessWidget {
   final TextEditingController _messageController = TextEditingController();
 
   //chat & auth services
-  final ChatServices _chatServices = ChatServices();
+  final ChatServices _chatService = ChatServices();
   final AuthService _authService = AuthService();
 
   //send message
@@ -19,7 +20,7 @@ class ChatPage extends StatelessWidget {
     //if there is something inside text Field
     if (_messageController.text.isNotEmpty) {
       //send message
-      await _chatServices.sendMessage(receiverId, _messageController.text);
+      await _chatService.sendMessage(receiverId, _messageController.text);
       //clear text controller
       _messageController.clear();
     }
@@ -34,9 +35,42 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           //display all the message
-          Expanded(child: _buildMessageList(),)
+          Expanded(
+            child: _buildMessageList(),
+          )
         ],
       ),
     );
+  }
+
+  //build message list
+  Widget _buildMessageList() {
+    String senderId = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+      stream: _chatService.getMessages(receiverId, senderId),
+      builder: (context, snapshot) {
+        //errors
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
+
+        //return listview
+        return ListView(
+          children:
+              snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+        );
+      },
+    );
+  }
+
+  //build message item
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Text(data["message"]);
   }
 }
